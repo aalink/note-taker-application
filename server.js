59995266420c1edb825f1938ad6678563510a945
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-16 22:54:33 -0400

    Comments, formatting, and console.log

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-// const apiRoutes = require("./routes/apiroutes")
-const htmlRoutes = require("./routes/htmlroutes")
-const express = require("express")
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlroutes");
+const express = require("express");
 
 const app = express();
-
-const PORT = process.env.PORT || 3001
+// Port for Heroku
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended:true
-}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+// API and HTML routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// app.use("/api", apiRoutes)
-app.use("/", htmlRoutes)
-
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+console.log("Visit page here: http://localhost:3001/");
 
 // Make data routes and HTML routes
-
