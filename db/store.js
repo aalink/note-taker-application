c3ce7cc23a40aa8a83f15a63eba9e7f2d791e248
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-17 17:08:45 -0400

    Making Store class

---
+++ db/store.js
@@ -0,0 +1,37 @@
+// Functions that retrieve data from the database.
+
+// Module Imports
+const fs = require("fs");
+const util = require("util");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+      return parsedNotes;
+    });
+  }
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+  addNote(note) {
+    const { title, text } = note;
+    const newNote = { title, text }
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote)
+  }
+}
+
+module.exports = new Store();
